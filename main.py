b8c60d23729e855909e9e7ac8695a597660b4b44
Dave Arter <github@davea.me>
2016-12-07 13:46:48 +0000

    Barebones script for sending data to MQTT

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from time import sleep
+
+from w1thermsensor import W1ThermSensor
+import paho.mqtt.publish as publish
+
+BROKER="10.0.1.216"
+TOPIC="sensor/temperature/utility"
+DELAY=10
+
+def main():
+    sensor = W1ThermSensor()
+    while True:
+        temperature = sensor.get_temperature()
+        print("Temperature: {}".format(temperature))
+        publish.single(TOPIC, str(temperature), hostname=BROKER)
+        print("Sleeping for {} seconds...".format(DELAY))
+        sleep(DELAY)
+
+if __name__ == '__main__':
+    main()
